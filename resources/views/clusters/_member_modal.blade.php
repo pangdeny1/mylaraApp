63fb02e2c74e0b290828de942a9658501f7ec8b9
David Pella <daviejaco@gmail.com>
2018-08-13 17:06:54 +0300

    Change button size from large to small

--- resources/views/clusters/_member_modal.blade.php
+++ resources/views/clusters/_member_modal.blade.php
@@ -50,7 +50,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
